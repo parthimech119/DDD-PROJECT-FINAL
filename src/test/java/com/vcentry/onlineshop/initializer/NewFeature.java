8372c5dff363ce967241d1ba08bb79a2ea86c8e0
parthimech119 <parthimech119@gmail.com>
2020-12-03 17:15:16 +0530

    new fixbranch1

--- src/test/java/com/vcentry/onlineshop/initializer/NewFeature.java
+++ src/test/java/com/vcentry/onlineshop/initializer/NewFeature.java
@@ -1,9 +1,13 @@
 package com.vcentry.onlineshop.initializer;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NewFeature {
+public class NewFeature extends Intializer {
 
-	WebDriver wd=new FirefoxDriver();
+	public static void featureNew(){
+	 wd=new FirefoxDriver();
+	 wd=new ChromeDriver();
+	}
 }
