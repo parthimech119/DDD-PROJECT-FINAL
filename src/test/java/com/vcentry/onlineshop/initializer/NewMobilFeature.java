571343b2c99869bcfda6342dac176858cbfffde1
parthimech119 <parthimech119@gmail.com>
2020-12-03 17:11:44 +0530

    new mobile feature

---
+++ src/test/java/com/vcentry/onlineshop/initializer/NewMobilFeature.java
@@ -0,0 +1,11 @@
+package com.vcentry.onlineshop.initializer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NewMobilFeature {
+
+	
+	WebDriver wd=new FirefoxDriver();
+	
+}
