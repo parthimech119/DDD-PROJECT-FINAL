700a4bbd0f262fb7efb6037ee3e237a48f693516
parthimech119 <parthimech119@gmail.com>
2020-12-03 17:05:08 +0530

    new fix

--- src/test/java/com/vcentry/onlineshop/initializer/NewFeature.java
+++ src/test/java/com/vcentry/onlineshop/initializer/NewFeature.java
@@ -1,5 +1,9 @@
 package com.vcentry.onlineshop.initializer;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class NewFeature {
 
+	WebDriver wd=new FirefoxDriver();
 }
